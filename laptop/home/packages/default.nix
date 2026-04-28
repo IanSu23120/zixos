7a562af1d20721e6f2d37f82b4ffb9a08a90c4a2
iansu <4B1L0901@stust.edu.tw>
2026-04-28 23:40:00 +0800

    add vscode

--- laptop/home/packages/default.nix
+++ laptop/home/packages/default.nix
@@ -4,17 +4,16 @@
     ./alacritty.nix
     ./apps.nix
     ./browser.nix
-    # ./direnv.nix
+    ./direnv.nix
     ./git.nix
     ./kitty.nix
     ./starship.nix
     ./tmux.nix
+    ./vscode.nix
     ./zed-editor.nix
     ./zsh.nix
   ];
   home.packages = with pkgs; [
-    direnv
-    devenv
 
     rustdesk-flutter
     discord
